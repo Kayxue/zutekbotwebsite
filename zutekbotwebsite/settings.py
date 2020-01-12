897d4a2504943e2382ec35d6442b2fa7b4ee3585
Kayxue <s0990006@gmail.com>
2020-01-12 12:04:39 +0800

    2020-01-12 Manjaro Linux

--- zutekbotwebsite/settings.py
+++ zutekbotwebsite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$+#(#wrhgc1g&bm9@y%&$_ua41t^njmi$+*3epm^!(883+hbiz'
+SECRET_KEY = '1d^ra^$eh6w783g_d$%x3f(bi&2knea8muy-wib29$cbnm$lwx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
