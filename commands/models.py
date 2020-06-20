d7be24d9aeb7977fedf021eab1c5a8433dffa334
Kayxue <s0990006@gmail.com>
2020-06-20 16:10:58 +0800

    2020-06-20

--- commands/models.py
+++ commands/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Command(models.Model):
+
+    command_name=models.CharField(max_length=20,verbose_name="指令名稱")
+
+    command_usage=models.CharField(max_length=100,verbose_name="指令用法")
+
+    command_info=models.CharField(max_length=100,verbose_name="指令說明")
+
+    create_date=models.DateField(verbose_name="指令完成時間",auto_now_add=False)
+
+    def __str__(self):
+        return self.command_name+" "+self.command_usage+" "+self.command_info
--- commands/views.py
+++ commands/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import *
+from .models import *
 
 # Create your views here.
 
 class AllCommand(TemplateView):
-    template_name = "allcommand.html"
+    template_name="allcommand.html"
